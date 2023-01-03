bf9b9be63813ffcaf45075948c20813d1369a3b2
Atiq ur Rehman <atiqu555@gmail.com>
2023-01-03 09:30:55 +0500

    request0change

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => {
-  res.send("faefae");
+  res.send("Chat gpt server running");
 });
 app.post("/", async (req, res) => {
   try {
